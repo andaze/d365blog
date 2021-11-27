9201229e668c66c463d640cf8dc9f712db3b9e01
bhavya <developer.ameio@gmail.com>
2021-11-27 12:00:44 +0530

    update api for language wise index send in algolia

--- api/indexUpdate.php
+++ api/indexUpdate.php
@@ -12,16 +12,20 @@ $index_en = $client->initIndex('d365_english_content');
 $index_ja = $client->initIndex('d365_japanese_content');
 $records = json_decode(file_get_contents('https://www.andaze.com/ja/dynamics365/algolia.json'), true);
 
-foreach ($records as $value) {
-  if($value['lang'] == "en")
-  {
-    $index_en->saveObjects($value, ['autoGenerateObjectIDIfNotExist' => true]);
+$en_result = array_filter($records, function ($item) {
+  if (stripos($item['lang'], 'en' ) !== false) {
+      return true;
   }
-  if($value['lang'] == "ja")
-  {
-    $index_ja->saveObjects($value, ['autoGenerateObjectIDIfNotExist' => true]);
+  return false;
+});
+
+$ja_result = array_filter($records, function ($item) {
+  if (stripos($item['lang'], 'ja' ) !== false) {
+      return true;
   }
-}
-// Batching is done automatically by the API client
-// $index->saveObjects($records, ['autoGenerateObjectIDIfNotExist' => true]);
+  return false;
+});
+
+$index_en->saveObjects($en_result, ['autoGenerateObjectIDIfNotExist' => true]);
+$index_ja->saveObjects($ja_result, ['autoGenerateObjectIDIfNotExist' => true]);
 ?>
